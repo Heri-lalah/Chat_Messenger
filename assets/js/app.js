31c71a7f5f9403df5aaa113c880c6235f6fce1be
Herilala <herilalah.rakotonasolo@gmail.com>
2022-11-09 14:20:49 +0300

    add style selected conversation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
     var conversation_activeClass = "border-l-2 border-red-600 bg-gradient-to-r from-red-300 to-red-100 transition-opacity";
     
     //Addclass
-    $(".conversation_item").each(function(index){
+    $(conversation_item).each(function(index){
         $(this).on("click",function(){
 
-            $(".conversation_item").each(function(index){
+            $(conversation_item).each(function(index){
                  $(conversation_item[index]).removeClass(conversation_activeClass);
             });
 
