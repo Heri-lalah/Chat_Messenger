93ffe84fda810a48fc4ebafab456b010ceb3dc59
Herilala <herilalah.rakotonasolo@gmail.com>
2022-11-09 14:19:38 +0300

    add style selected conversation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    alert("test");
+    //Animation in sidebar2 conversation  list
+    var conversation_item = document.querySelectorAll('.conversation_item');
+    var conversation_activeClass = "border-l-2 border-red-600 bg-gradient-to-r from-red-300 to-red-100 transition-opacity";
+    
+    //Addclass
+    $(".conversation_item").each(function(index){
+        $(this).on("click",function(){
+
+            $(".conversation_item").each(function(index){
+                 $(conversation_item[index]).removeClass(conversation_activeClass);
+            });
+
+            $(this).addClass(conversation_activeClass);
+        })
+    });
 })
