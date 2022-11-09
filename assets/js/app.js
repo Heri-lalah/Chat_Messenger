e405ecdc9599c27f72f068016f6eae5383d768db
Herilala <herilalah.rakotonasolo@gmail.com>
2022-11-09 18:30:33 -0500

    add app.js, change color layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,32 @@
-$(document).ready(function(){
-    //Animation in sidebar2 conversation  list
-    var conversation_item = document.querySelectorAll('.conversation_item');
-    var conversation_activeClass = "border-l-2 border-red-600 bg-gradient-to-r from-red-300 to-red-100 transition-opacity";
-    
-    //Addclass
-    $(conversation_item).each(function(index){
+function clickedElement(cible, addClass) {
+    value = $(cible).each(function(index){
+
         $(this).on("click",function(){
-
-            $(conversation_item).each(function(index){
-                 $(conversation_item[index]).removeClass(conversation_activeClass);
+    
+            $(cible).each(function(index){
+                 $(cible[index]).removeClass(addClass);
             });
-
-            $(this).addClass(conversation_activeClass);
+    
+            $(this).addClass(addClass);
         })
     });
+
+    return value;
+}
+
+
+$(document).ready(function(){
+    //conversation list
+    var conversation_item = document.querySelectorAll('.conversation_item');
+    var conversation_activeClass = "border-l-2 border-red-600 bg-gradient-to-r from-red-300 to-red-100 transition-opacity";
+    clickedElement(conversation_item, conversation_activeClass);
+
+    var boxType = document.querySelectorAll('.boxType');
+    var boxTypeClassActive = "bg-indigo-300 text-white";
+    clickedElement(boxType, boxTypeClassActive);
+
+    var AllconversationType = document.querySelectorAll('.conversation_type_item');
+    var activeConversationTypeClass = "text-indigo-800";
+    clickedElement(AllconversationType, activeConversationTypeClass);
+
 })
